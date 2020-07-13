dfa88a3d49f088998af8af1896ab66b2262d1171
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-13 17:47:06 +0900

    feat: signup to insert User data

--- backend/routes/signup.ts
+++ backend/routes/signup.ts
@@ -1,6 +1,14 @@
-const express = require('express');
+import express = require('express');
 const router = express.Router();
+const { insertUser } = require("../database/database.ts");
 
-router.post('/', function signup(request: Request, response: Response) {
-    const body: object = request.body;
+router.post('/', function signUp(request: express.Request, response: express.Response) {
+    const body: any = JSON.parse(JSON.stringify(request.body));
+    const data = insertUser(body.id, body);
+    response.status(200).send({
+        status: "200",
+        data: data
+    });
 })
+
+module.exports = router;
