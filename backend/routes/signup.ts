93b4d3691a4c5546ca8b65323f1f23c0b1f8d7d8
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-13 13:59:56 +0900

    chore: basic setup done by webstorm

---
+++ backend/routes/signup.ts
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/', function signup(request: Request, response: Response) {
+    const body: object = request.body;
+})
