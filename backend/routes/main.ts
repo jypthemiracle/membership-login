54e1092ce91a5a9274b0e446cc5c56017495a947
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-13 21:34:59 +0900

    refactor: router main to junction sessionId existance

--- backend/routes/main.ts
+++ backend/routes/main.ts
@@ -6,13 +6,16 @@ const {getUserBySession} = require('../database/database.ts')
 
 router.get('/', (request: express.Request, response: express.Response) => {
     const sessionId: string = request.cookies.sessionId;
-    const userData: User = getUserBySession(sessionId);
-    if (userData) {
-        response.status(200).send({
-            status: "OK",
-            body: userData,
-        });
+    if (sessionId) {
+        const userData: User = getUserBySession(sessionId);
+        if (userData) {
+            response.status(200).send({
+                status: "OK",
+                body: userData,
+            });
+        }
     }
+    return;
 })
 
 module.exports = router;
