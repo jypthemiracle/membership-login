7532a0519ba133527f9df4a35090796aca068035
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-13 21:27:14 +0900

    feat: add main router

---
+++ backend/routes/main.ts
@@ -0,0 +1,18 @@
+import express = require('express');
+import {User} from "../model/User";
+const router = express.Router();
+
+const {getUserBySession} = require('../database/database.ts')
+
+router.get('/', (request: express.Request, response: express.Response) => {
+    const sessionId: string = request.cookies.sessionId;
+    const userData: User = getUserBySession(sessionId);
+    if (userData) {
+        response.status(200).send({
+            status: "OK",
+            body: userData,
+        });
+    }
+})
+
+module.exports = router;
